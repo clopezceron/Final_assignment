0b057b92c16a436f470ba47124c1bbe6def254fa
clopezceron <c.lopezceron@student.uw.edu.pl>
2026-06-03 22:30:26 +0200

    Update implementacion de lematizador.py

--- recopilacion_palabras.py
+++ recopilacion_palabras.py
@@ -1,17 +1,28 @@
 def crear_diccionario(libro):
     with open(libro, "r", encoding="utf-8") as file:
         texto=file.read().lower()
-        
+
     import re
     texto_limpio = re.sub(r'[^\w\s]', '', texto)
 
+
     texto_lista=texto_limpio.split()
-    diccionario=dict.fromkeys(texto_lista)
+
+    import spacy
+    nlp = spacy.blank("es")
+    nlp.add_pipe("lemmatizer", config={"mode": "lookup"})
+    nlp.initialize()
+    doc = nlp(" ".join(texto_lista))
+    lista_lemas = [token.lemma_ for token in doc]
+
+    diccionario=dict.fromkeys(lista_lemas)
     
     import requests
     for palabra in diccionario:
         req = requests.get(f"https://rae-api.com/api/words/{palabra}")
         diccionario[palabra] = req.json()
-    return diccionario
+
+    
+    return diccionario, texto_lista
 
 
