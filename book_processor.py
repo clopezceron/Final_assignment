b1f0c884a453bd6c62a138aa18ead722821ae76d
clopezceron <c.lopezceron@student.uw.edu.pl>
2026-06-11 21:31:59 +0200

    Update book_processor.py

--- book_processor.py
+++ book_processor.py
@@ -4,8 +4,6 @@ nlp.add_pipe("lemmatizer", config={"mode": "lookup"})
 nlp.initialize()
 
 def process_book(libro):
-    with open(libro, "r", encoding="utf-8") as file:
-        libro=file.read().lower()
     signos_puntuacion=['.',',',';',':','¿','?','¡','!','(',')','[',']','—','«','»','"',"'",'¨','/','*','{','}','_','^','~','•','°','%','&','+','<','>','|','$','1','2','3','4','5','6','7','8','9','0']
     for signo in signos_puntuacion:
         libro = libro.replace(signo, ' ')
