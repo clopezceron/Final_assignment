d7bd84a50d4f289f574ad804c9279dffc3afface
clopezceron <c.lopezceron@student.uw.edu.pl>
2026-06-11 23:02:47 +0200

    Update dictionary_processor.py

--- dictionary_processor.py
+++ dictionary_processor.py
@@ -1,4 +1,9 @@
-def process_dictionary(dictionary):
-  with open(dictionary, "r", encoding="utf-8") as file:
-        lista_palabras=file.read().lower().split()
-    return lista_palabras
+def get_frecuencias(libro_normal,libro_lematizado):
+#devuelve diccionarios con palabra/lema : frecuencia
+    frecuencias=dict.fromkeys(libro_normal)
+    frecuencias_lemas=dict.fromkeys(libro_lematizado)   
+    for palabra in frecuencias:
+        frecuencias[palabra]=libro_normal.count(palabra)
+    for palabra in frecuencias_lemas:
+        frecuencias_lemas[palabra]=libro_lematizado.count(palabra)  
+    return frecuencias, frecuencias_lemas
