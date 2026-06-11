08f6aca149ae51b119a6a2c0d155783e01de3369
clopezceron <c.lopezceron@student.uw.edu.pl>
2026-06-11 21:01:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 
+import argparse
+
+parser = argparse.ArgumentParser(description="Literature Analyzer")
+parser.add_argument("--dictionary",required=True, help="Dictionary file")
+parser.add_argument("--works",required=True,help="Comma-separated list of works")
+parser.add_argument("--dictionary-stats",action="store_true",help="Show dictionary statistics")
+parser.add_argument("--no-words",action="store_true",help="Show words not present in dictionary")
+parser.add_argument("--frequencies",type=int,help="Show the n most frequent words")
+
+args = parser.parse_args()
