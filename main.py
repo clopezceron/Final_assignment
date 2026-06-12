bb7bb94e3c9e02c5ffc157e614e761bfe22419d9
clopezceron <c.lopezceron@student.uw.edu.pl>
2026-06-12 15:06:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -17,16 +17,25 @@ args = parser.parse_args()
         diccionario=file.read().lower().split()
   
 works_files = args.works.split(",")
-estadisticas={}
+conjunto_frecuencias={}
+conjunto_frecuencias_lemas={}
+conjunto_numero_palabras={}
+conjunto_palabras_unicas={}
+conjunto_lemas_unicos={}
+conjunto_top_palabras={}
+conjunto_top_lemas={}
 for work in works_files:
     with open(work, "r", encoding="utf-8") as file:
          libro=file.read().lower()
     libro_limpio=process_book(libro)
     #libro en forma de lista con palabras lematizadas[0] y originales[1], y en forma de string lemetizado[2] y original[3]
     frecuencias, frecuencias_lemas=get_frecuencias(libro_limpio[1],libro_limpio[0])
+    conjunto_frecuencias[work]=frecuencias
+    conjunto_frecuencias_lemas[work]=frecuencias_lemas
     if args.dictionary_stats:
-        print('The statistics for the book ',work ,' are as follows') 
-        estadisticas[work]=show_statistics(frecuencias, frecuencias_lemas, diccionario)
-    if args.dictionary_stats and len(work_files)>1:
-        show_total_statistics(estadisticas)
+        print('The statistics for the file ',work ,' are as follows') 
+        conjunto_numero_palabras[work],conjunto_palabras_unicas[work],conjunto_lemas_unicos[work],conjunto_top_palabras[work],conjunto_top_lemas[work]=show_statistics(frecuencias, frecuencias_lemas, diccionario)
+        
+if args.dictionary_stats and len(work_files)>1:
+    show_total_statistics(conjunto_numero_palabras,conjunto_palabras_unicas,conjunto_lemas_unicos, conjunto_top_palabras,conjunto_top_lemas,conjunto_frecuencias, conjunto_frecuencias_lemas)
         
