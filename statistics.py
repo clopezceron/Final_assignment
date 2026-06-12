dccef5b2c218bcef13c908fb345d8f62c14e8a31
clopezceron <c.lopezceron@student.uw.edu.pl>
2026-06-12 17:34:01 +0200

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -1,5 +1,5 @@
 from tops_obtainer import get_top
-def make_statistics(frecuencias, frecuencias_lemas, diccionario):
+def make_statistics(frecuencias, frecuencias_lemas, diccionario, libro_en_string):
     numero_palabras=sum(frecuencias.values())
     numero_palabras_distintas=len(frecuencias)              
     numero_lemas_distintos=len(frecuencias_lemas)
@@ -15,7 +15,19 @@ def make_statistics(frecuencias, frecuencias_lemas, diccionario):
     numero_lemas_unicos=j        
     top_palabras, top_lemas=get_top(frecuencias, frecuencias_lemas, 10)
     numero_palabras_diccionario=len(diccionario)
-    return numero_palabras_diccionario,numero_palabras,numero_palabras_distintas,numero_lemas_distintos,numero_palabras_unicas, numero_lemas_unicos, top_palabras, top_lemas
+    
+    abc_lista=[' ','a','b','c','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z', 'á', 'é', 'í', 'ó','ú']
+    abc=dict.fromkeys(abc_lista)
+    nuevos_caracteres={}
+    libro_en_lista=list(libro_en_string)
+    for letra in abc_lista:
+        abc[letra]=libro_en_lista.count(letra)
+  
+    for letra in libro_en_lista:
+        if letra not in abc_lista or letra not in nuevos_caracteres:
+            nuevos_caracteres[letra]=libro_en_lista.count(letra)
+    
+    return numero_palabras_diccionario,numero_palabras,numero_palabras_distintas,numero_lemas_distintos,numero_palabras_unicas, numero_lemas_unicos, top_palabras, top_lemas, abc, nuevos_caracteres
 
 def show_single_statistics(file,numero_palabras_diccionario,numero_palabras,numero_palabras_distintas,numero_lemas_distintos,numero_palabras_unicas, numero_lemas_unicos, top_palabras, top_lemas):
     print('The statistics for the file ', file,' are below')
