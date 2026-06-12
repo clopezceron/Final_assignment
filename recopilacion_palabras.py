7055b253373aab820da15a629a2bc9f4d67fd408
clopezceron <c.lopezceron@student.uw.edu.pl>
2026-06-12 20:39:06 +0200

    Update recopilacion_palabras.py

--- recopilacion_palabras.py
+++ recopilacion_palabras.py
@@ -1,25 +1,4 @@
-import re
-import spacy
-import requests
-
-def crear_diccionario(libro):
-    with open(libro, "r", encoding="utf-8") as file:
-        texto=file.read().lower()
-
-   
-    texto_limpio = re.sub(r'[^\w\s]', '', texto)
-
-
-    texto_lista=texto_limpio.split()
-
-    nlp = spacy.blank("es")
-    nlp.add_pipe("lemmatizer", config={"mode": "lookup"})
-    nlp.initialize()
-    doc = nlp(" ".join(texto_lista))
-    lista_lemas = [token.lemma_ for token in doc]
-
-    diccionario=dict.fromkeys(lista_lemas)
-    
+import requests  
     for palabra in diccionario:
         req = requests.get(f"https://rae-api.com/api/words/{palabra}")
         diccionario[palabra] = req.json()
