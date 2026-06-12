a3772de7833758454c0cfe1d880c43d7c9ddad43
clopezceron <c.lopezceron@student.uw.edu.pl>
2026-06-12 14:29:40 +0200

    Update statistics.py

--- statistics.py
+++ statistics.py
@@ -1,3 +1,4 @@
+import numpy as np
 from tops_obtainer import get_top
 def show_statistics(frecuencias, frecuencias_lemas, diccionario):
     print("The dictionary used cointain ", len(diccionario)," words")
@@ -9,13 +10,13 @@ def show_statistics(frecuencias, frecuencias_lemas, diccionario):
     print("The numbrer of different lemmas in the book is: ",numero_lemas_distintos )
     i=0  
     for palabra in frecuencias:
-        if frecuencias[palabra]==0:
+        if frecuencias[palabra]==1:
             i+=1
     numero_palabras_unicas=i
     print("The number of unique words in the book is: ", numero_palabras_unicas)
     j=0
     for palabra in frecuencias_lemas:
-        if frecuencias_lemas[palabra]==0:
+        if frecuencias_lemas[palabra]==1:
             j+=1
     numero_lemas_unicos=j        
     print("The number of unique lemas in the book is: ", numero_lemas_unicos)
@@ -23,5 +24,6 @@ def show_statistics(frecuencias, frecuencias_lemas, diccionario):
     print("The 10 most used words with the amount of times used are: ",top_palabras)
     print("The 10 most used lemas with the amount of times used are: ",top_lemas)
     return numero_palabras,numero_palabras_distintas,numero_lemas_distintos,numero_palabras_unicas, numero_lemas_unicos, top_palabras,top_lemas
+
     
     
