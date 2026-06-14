5ddac1c1a26942c6c61582f5f0ea03a853d6c07f
Citlali López Cerón <90635825+CitiLopez@users.noreply.github.com>
2026-06-14 20:13:55 +0200

    Update similarity_function.py

--- similarity_function.py
+++ similarity_function.py
@@ -1,5 +1,6 @@
 import numpy as np
 from tops_obtainer import get_top
+from statistics import make_statistics
 def coseno_angulo(u,v):
     producto_punto = np.dot(u, v)
     norma_u = np.linalg.norm(u)
@@ -15,7 +16,9 @@ def crear_vectores(abecedario,frecuencias, frecuencias_lemas, palabras_totales):
      
   
 def comparar2a2(frecuencias1, frecuencias_lemas1,frecuencias2, frecuencias_lemas2,diccionario, libro_en_string1,libro_en_string2 ):
-    numero_palabras_diccionario, palabras_totales1,numero_palabras_distintas1,numero_lemas_distintos1,numero_palabras_unicas1, numero_lemas_unicos1, top_palabras1, top_lemas1, abcdario1, nuevos_caracteres1 =make_statistics(frecuencias1, frecuencias_lemas1, diccionario, libro_en_string1)
+    numero_palabras_diccionario, palabras_totales1,numero_palabras_distintas1,numero_lemas_distintos1,numero_palabras_unicas1, numero_lemas_unicos1, top_palabras1, top_lemas1, abecdario1, nuevos_caracteres1 =make_statistics(frecuencias1, frecuencias_lemas1, diccionario, libro_en_string1)
+    numero_palabras_diccionario, palabras_totales2,numero_palabras_distintas2,numero_lemas_distintos2,numero_palabras_unicas2, numero_lemas_unicos2, top_palabras2, top_lemas2, abecdario2, nuevos_caracteres2 =make_statistics(frecuencias2, frecuencias_lemas2, diccionario, libro_en_string2)
+    
     vector_top_palabras1, vector_top_lemas1, vector_abc1= crear_vectores(abecedario1,frecuencias1, frecuencias_lemas1, palabras_totales1)
     vector_top_palabras2, vector_top_lemas2, vector_abc2= crear_vectores(abecedario2,frecuencias2, frecuencias_lemas2, palabras_totales2)
     numero_palabras_nuevas1=len(nuevos_caracteres1)
@@ -29,8 +32,8 @@ def comparar2a2(frecuencias1, frecuencias_lemas1,frecuencias2, frecuencias_lemas
     puntuacion5= 1- abs((numero_palabras_unicas1/palabras_totales1)-( numero_palabras_unicas2/palabras_totales2))*10
     puntuacion6= 1- abs((numero_lemas_totales1/palabras_totales1)-(numero_lemas_totales2/palabras_totales2))*10
     puntuacion7= 1- abs((numero_palabras_nuevas1/palabras_totales1)-(numero_palabras_nuevas2/palabras_totales2))*10
-    puntuacion_total: puntuacion1+ puntuacion2 + puntuacion3+ puntuacion4+ puntuacion5 + puntuacion6+ puntuacion7
-    return    puntuacion_total
+    puntuacion_total= puntuacion1+ puntuacion2 + puntuacion3+ puntuacion4+ puntuacion5 + puntuacion6+ puntuacion7
+    return puntuacion_total
 
 def 
     
