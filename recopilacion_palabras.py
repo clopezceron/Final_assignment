a464ec55281120366ddbac7dbc1e01b67ba9ad3a
Citlali López Cerón <90635825+CitiLopez@users.noreply.github.com>
2026-06-14 17:53:48 +0200

    Delete recopilacion_palabras.py

--- recopilacion_palabras.py
+++
@@ -1,9 +0,0 @@
-import requests  
-    for palabra in diccionario:
-        req = requests.get(f"https://rae-api.com/api/words/{palabra}")
-        diccionario[palabra] = req.json()
-
-    
-    return diccionario, texto_lista
-
-
