549f2c0ed45b65e71f8ad1f0895b32030d1da738
Citlali López Cerón <90635825+CitiLopez@users.noreply.github.com>
2026-06-14 14:36:51 +0200

    Update no_words.py

--- no_words.py
+++ no_words.py
@@ -1,9 +1,9 @@
 def find_new_words(libro_limpio_forma_lista,diccionario_forma_lista):
     palabras_nuevas={}
     for palabra in libro_limpio_forma_lista:
-        if palabra not in diccionario_forma_lista or palabra not in palabras_nuevas:
+        if palabra not in diccionario_forma_lista and palabra not in palabras_nuevas:
             palabras_nuevas[palabra]=libro_limpio_forma_lista.count(palabra)
-     print('The words used by the master that do not exist in the current dictionary with their respective number of ocurrences are: ')   
-     print(palabras_nuevas)
+    print('The words used by the master that do not exist in the current dictionary with their respective number of ocurrences are: ')   
+    print(palabras_nuevas)
 
       
