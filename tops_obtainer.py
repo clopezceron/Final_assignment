586b6bb5443ac1491be5aae1023502ebfc7d1cf8
Citlali López Cerón <90635825+CitiLopez@users.noreply.github.com>
2026-06-14 14:55:44 +0200

    Update tops_obtainer.py

--- tops_obtainer.py
+++ tops_obtainer.py
@@ -1,13 +1,13 @@
 def get_top(frecuencias, frecuencias_lemas,n): 
     #devuelve diccionario con n palabras/lemas: cantidades de palabras/ lemas más repetidos
     lista_frecuencias=list(frecuencias.values())
-    lista_frecuencias.sort()
+    lista_frecuencias.sort(reverse=True)
     top_palabras=lista_frecuencias[0:n]
     lista_frecuencias_lemas=list(frecuencias_lemas.values())
-    lista_frecuencias_lemas.sort()
+    lista_frecuencias_lemas.sort(reverse=True)
     top_lemas=lista_frecuencias_lemas[0:n]
     dic_top_palabras={}
-    dic_top_frecuencias={}
+    dic_top_lemas={}
     for i in top_palabras:
         if i==0:
             print("No more words to show")
@@ -15,15 +15,15 @@ def get_top(frecuencias, frecuencias_lemas,n):
         else:
             for palabra in frecuencias:
                 if frecuencias[palabra]==i:
-                    dict_top_palabras[palabra]=i
-     for i in top_lemas:
-         if i==0:
-             print("No more lemas to show")
-             break
-         else:
-             for palabra in frecuencias_lemas:
-                 if frecuencias_lemas[palabra]==i:
-                     dict_top_frecuencias[palabra]=i                       
+                    dic_top_palabras[palabra]=i
+    for i in top_lemas:
+        if i==0:
+            print("No more lemas to show")
+            break
+        else:
+            for palabra in frecuencias_lemas:
+                if frecuencias_lemas[palabra]==i:
+                    dic_top_lemas[palabra]=i                       
                             
         
     return dic_top_palabras, dic_top_lemas
