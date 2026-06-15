a2256c136f6c6731f9620d191158575efc7c933c
clopezceron <c.lopezceron@student.uw.edu.pl>
2026-06-15 19:36:57 +0200

    Update similarity_function.py

--- similarity_function.py
+++ similarity_function.py
@@ -8,7 +8,7 @@ def coseno_angulo(u,v):
     cos_theta = producto_punto / (norma_u * norma_v)
     return cos_theta
 def crear_vectores(abecedario,frecuencias, frecuencias_lemas, palabras_totales):
-    top_palabras, top_lemas=get_top(frecuencias, frecuencias_lemas, 150)
+    top_palabras, top_lemas=get_top(frecuencias, frecuencias_lemas, 200)
     vector_top_palabras =np.array(list(top_palabras.values()))*100/palabras_totales
     vector_top_lemas =np.array(list(top_lemas.values()))*100/palabras_totales
     vector_abc =np.array(list(abecedario.values()))*100/palabras_totales
@@ -16,8 +16,21 @@ def crear_vectores(abecedario,frecuencias, frecuencias_lemas, palabras_totales):
      
   
 def compare(frecuencias1, frecuencias_lemas1,frecuencias2, frecuencias_lemas2,diccionario, libro_en_string1,libro_en_string2 ):
+        
+    palabras_vacias=["el", "la", "los", "las", "un", "una", "unos", "unas","a", "ante","bajo", "cabe", "con", "contra", "de", "desde","durante", "en", "entre","hacia", "hasta", "para", "por","según", "sin", "sobre", "tras", "al", "del","y", "e", "ni", "o", "u", "pero", "aunque", "sino","yo", "tú", "tu", "vos", "él", "ella", "ello","nos", "nosotros", "nosotras","vosotros","vosotras","ellos", "ellas","me", "te", "se", "le", "les", "lo", "la", "los", "las","que","quien","quienes", "cual", "cuales", "cuyo", "cuyos","donde", "cuando", "como", "cuanto","ser", "estar", "haber","ya", "muy", "más", "menos", "tan", "todo", "toda", "todos", "todas",
+    "cada", "casi", "también", "sí", "no", "si"]
+    for palabra in palabras_vacias:
+        if palabra in frecuencias1:
+            del frecuencias_lemas1[palabra]
+        if palabra in frecuencias2:
+            del frecuencias_lemas2[palabra]
+        if palabra in frecuencias2:
+            del frecuencias1[palabra]
+ 
     numero_palabras_diccionario, palabras_totales1,numero_palabras_distintas1,numero_lemas_distintos1,numero_palabras_unicas1, numero_lemas_unicos1, top_palabras1, top_lemas1, abecedario1, nuevos_caracteres1 =make_statistics(frecuencias1, frecuencias_lemas1, diccionario, libro_en_string1)
     numero_palabras_diccionario, palabras_totales2,numero_palabras_distintas2,numero_lemas_distintos2,numero_palabras_unicas2, numero_lemas_unicos2, top_palabras2, top_lemas2, abecedario2, nuevos_caracteres2 =make_statistics(frecuencias2, frecuencias_lemas2, diccionario, libro_en_string2)
+            
+   
     
     vector_top_palabras1, vector_top_lemas1, vector_abc1= crear_vectores(abecedario1,frecuencias1, frecuencias_lemas1, palabras_totales1)
     vector_top_palabras2, vector_top_lemas2, vector_abc2= crear_vectores(abecedario2,frecuencias2, frecuencias_lemas2, palabras_totales2)
@@ -29,11 +42,10 @@ def compare(frecuencias1, frecuencias_lemas1,frecuencias2, frecuencias_lemas2,di
     puntuacion2=coseno_angulo(vector_top_lemas1, vector_top_lemas2)*(100/7)
     puntuacion3=coseno_angulo(vector_abc1, vector_abc2)*(100/7)
     puntuacion4= 1- abs((numero_palabras_distintas1/palabras_totales1)-( numero_palabras_distintas2/palabras_totales2))*(100/7)
+    
     puntuacion5= 1- abs((numero_palabras_unicas1/palabras_totales1)-( numero_palabras_unicas2/palabras_totales2))*(100/7)
     puntuacion6= 1- abs((numero_lemas_totales1/palabras_totales1)-(numero_lemas_totales2/palabras_totales2))*(100/7)
     puntuacion7= 1- abs((numero_palabras_nuevas1/palabras_totales1)-(numero_palabras_nuevas2/palabras_totales2))*(100/7)
+ 
     puntuacion_total= puntuacion1+ puntuacion2 + puntuacion3+ puntuacion4+ puntuacion5 + puntuacion6+ puntuacion7
     return puntuacion_total
-
-
-    
