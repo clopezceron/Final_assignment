98826949fab7c1fe7089a2424bc09c0fdf957caa
clopezceron <c.lopezceron@student.uw.edu.pl>
2026-06-17 09:54:46 +0200

    Update statistics_code.py

--- statistics_code.py
+++ statistics_code.py
@@ -1,73 +1,60 @@
-import numpy as np
 from tops_obtainer import get_top
-from statistics_code import make_statistics
-def coseno_angulo(u,v):
-    producto_punto = np.dot(u, v)
-    norma_u = np.linalg.norm(u)
-    norma_v = np.linalg.norm(v)
-    cos_theta = producto_punto / (norma_u * norma_v)
-    return cos_theta
-def crear_vectores(abecedario,frecuencias, frecuencias_lemas, palabras_totales):
-    top_palabras, top_lemas=get_top(frecuencias, frecuencias_lemas, 100)
-    vector_top_palabras =np.array(list(top_palabras.values()))
-    vector_top_lemas =np.array(list(top_lemas.values()))
-    vector_abc =np.array(list(abecedario.values()))
-    return vector_top_palabras, vector_top_lemas, vector_abc   
-     
+def make_statistics(frecuencias, frecuencias_lemas, diccionario, libro_en_string):
+    numero_palabras=sum(frecuencias.values())
+    numero_palabras_distintas=len(frecuencias)              
+    numero_lemas_distintos=len(frecuencias_lemas)
+    i=0  
+    for palabra in frecuencias:
+        if frecuencias[palabra]==1:
+            i+=1
+    numero_palabras_unicas=i
+    j=0
+    for palabra in frecuencias_lemas:
+        if frecuencias_lemas[palabra]==1:
+            j+=1
+    numero_lemas_unicos=j        
+    top_palabras, top_lemas=get_top(frecuencias, frecuencias_lemas, 10)
+    numero_palabras_diccionario=len(diccionario)
+    
+    abc_lista=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z', 'á', 'é', 'í', 'ó','ú']
+    abc=dict.fromkeys(abc_lista)
+    nuevos_caracteres={}
+    libro_en_lista=list(libro_en_string)
+    for letra in abc_lista:
+        abc[letra]=libro_en_lista.count(letra)
   
-def compare(frecuencias1, frecuencias_lemas1,frecuencias2, frecuencias_lemas2,diccionario, libro_en_string1,libro_en_string2 ):
-        
-    palabras_vacias=["el", "la", "los", "las", "un", "una", "unos", "unas","a", "ante","bajo", "cabe", "con", "contra", "de", "desde","durante", "en",
-                     "entre","hacia", "hasta", "para", "por","según", "sin", "sobre", "tras", "al", "del","y", "e", "ni", "o", "u", "pero", "aunque", 
-                     "sino","yo", "tú", "tu", "vos", "él", "ella", "ello","nos", "nosotros", "nosotras","vosotros","vosotras","ellos", "ellas","me", "te",
-                     "se", "le", "les", "lo", "la", "los", "las","que","quien","quienes", "cual", "cuales", "cuyo", "cuyos","donde", "cuando", "como", "cuanto",
-                     "ser", "estar", "haber","ya", "muy", "más", "menos", "tan", "todo", "toda", "todos", "todas","cada", "casi", "también", "sí", "no", "si"]
+    for letra in libro_en_lista:
+        if letra not in abc_lista and letra not in nuevos_caracteres and letra!= " ":
+            nuevos_caracteres[letra]=libro_en_lista.count(letra)
+    
+    return numero_palabras_diccionario,numero_palabras,numero_palabras_distintas,numero_lemas_distintos,numero_palabras_unicas, numero_lemas_unicos, top_palabras, top_lemas, abc, nuevos_caracteres
 
-    for palabra in palabras_vacias:
-    
-        if palabra in frecuencias1:
-            del frecuencias1[palabra]
-            
-        if palabra in frecuencias2:
-            del frecuencias2[palabra]
-            
-        if palabra in frecuencias_lemas1:
-            del frecuencias_lemas1[palabra]
-                
-        if palabra in frecuencias_lemas2:
-            del frecuencias_lemas2[palabra]
-        
-            
-        
- 
-    numero_palabras_diccionario1, palabras_totales1,numero_palabras_distintas1,numero_lemas_distintos1,numero_palabras_unicas1, numero_lemas_unicos1, top_palabras1, top_lemas1, abecedario1, nuevos_caracteres1 =make_statistics(frecuencias1, frecuencias_lemas1, diccionario, libro_en_string1)
-    numero_palabras_diccionario2, palabras_totales2,numero_palabras_distintas2,numero_lemas_distintos2,numero_palabras_unicas2, numero_lemas_unicos2, top_palabras2, top_lemas2, abecedario2, nuevos_caracteres2 =make_statistics(frecuencias2, frecuencias_lemas2, diccionario, libro_en_string2)
-            
-   
-    
-    vector_top_palabras1, vector_top_lemas1, vector_abc1= crear_vectores(abecedario1,frecuencias1, frecuencias_lemas1, palabras_totales1)
-    vector_top_palabras2, vector_top_lemas2, vector_abc2= crear_vectores(abecedario2,frecuencias2, frecuencias_lemas2, palabras_totales2)
-    numero_palabras_nuevas1=len(nuevos_caracteres1)
-    numero_palabras_nuevas2= len(nuevos_caracteres2)
-    rpd1= numero_palabras_distintas1 / palabras_totales1
-    rpd2 = numero_palabras_distintas2 / palabras_totales2
-    rpu1 = numero_palabras_unicas1 / palabras_totales1
-    rpu2 = numero_palabras_unicas2 / palabras_totales2
-    rld1 = numero_lemas_distintos1 / palabras_totales1
-    rld2 = numero_lemas_distintos2 / palabras_totales2
-    
-    numero_lemas_totales1=sum(list(frecuencias_lemas1.values()))
-    numero_lemas_totales2=sum(list(frecuencias_lemas2.values()))
-    
-    #puntuacion1=coseno_angulo(vector_top_palabras1, vector_top_palabras2)
-    puntuacion2=coseno_angulo(vector_top_lemas1, vector_top_lemas2)
-    puntuacion3=coseno_angulo(vector_abc1, vector_abc2)
-    #puntuacion4= 1- abs(rpd1 - rpd2)
-    puntuacion5= 1- abs(rpu1 - rpu2)
-    puntuacion6= 1 - abs(rld1 - rld2)
-    #puntuacion7= 1- abs((numero_palabras_nuevas1/palabras_totales1)-(numero_palabras_nuevas2/palabras_totales2))
-    
-    #puntuacion_total= (puntuacion1+ puntuacion2 + puntuacion3+ puntuacion4+ puntuacion5 + puntuacion6+ puntuacion7)*(100/7)
-    puntuacion_total= (puntuacion2*70 + puntuacion3*10+ puntuacion5*10 + puntuacion6*10)
-    
-    return puntuacion_total
+def show_single_statistics(file,numero_palabras_diccionario,numero_palabras,numero_palabras_distintas,numero_lemas_distintos,numero_palabras_unicas, numero_lemas_unicos, top_palabras, top_lemas, abc, nuevos_caracteres):
+    print('The statistics for the file ', file,' are below')
+    print("The dictionary used cointain ", numero_palabras_diccionario," words")
+    print("The number of total words (and lemmas) in the book is: ", numero_palabras)
+    print("The number of different words in the book is: ",numero_palabras_distintas ) 
+    print("The numbrer of different lemmas in the book is: ",numero_lemas_distintos )
+    print("The number of unique words in the book is: ", numero_palabras_unicas)
+    print("The number of unique lemas in the book is: ", numero_lemas_unicos)
+    print("The 10 most used words with the amount of times used are: ",top_palabras)
+    print("The 10 most used lemas with the amount of times used are: ",top_lemas)
+    print('The used letters with the number of its ocurrences in the book are: ')
+    print(abc)
+    print('Other caracters found in the book are: ')
+    print(nuevos_caracteres)
+
+def show_total_statistics(numero_palabras_diccionario,numero_palabras,numero_palabras_distintas,numero_lemas_distintos,numero_palabras_unicas, numero_lemas_unicos, top_palabras, top_lemas, abc, nuevos_caracteres):   
+    print('The overall statistics for the books as a total for all files is below')
+    print("The dictionary used cointain ", numero_palabras_diccionario," words")
+    print("The overall number of words (and lemmas) in all of the books is: ", numero_palabras)
+    print("The overall number of different words in all of the books is: ",numero_palabras_distintas ) 
+    print("The overall number of different lemmas in all of the books is: ",numero_lemas_distintos )
+    print("The overall number of unique words in all of the books is: ", numero_palabras_unicas)
+    print("The overall number of unique lemas in all of the the books is: ", numero_lemas_unicos)
+    print("The 10 most used words with the amount of times used in all of the books are: ",top_palabras)
+    print("The 10 most used lemas with the amount of times used in all of the books are: ",top_lemas)
+    print('The used letters with the number of its ocurrences in all of the books are: ')
+    print(abc)
+    print('Other caracters found in the books are: ')
+    print(nuevos_caracteres)
