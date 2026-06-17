8a75a76b86ce5e64553e73e17048c394cc9f6dd6
clopezceron <c.lopezceron@student.uw.edu.pl>
2026-06-17 09:47:26 +0200

    Update similarity_function.py

--- similarity_function.py
+++ similarity_function.py
@@ -8,10 +8,10 @@ def coseno_angulo(u,v):
     cos_theta = producto_punto / (norma_u * norma_v)
     return cos_theta
 def crear_vectores(abecedario,frecuencias, frecuencias_lemas, palabras_totales):
-    top_palabras, top_lemas=get_top(frecuencias, frecuencias_lemas, 200)
-    vector_top_palabras =np.array(list(top_palabras.values()))*100/palabras_totales
-    vector_top_lemas =np.array(list(top_lemas.values()))*100/palabras_totales
-    vector_abc =np.array(list(abecedario.values()))*100/palabras_totales
+    top_palabras, top_lemas=get_top(frecuencias, frecuencias_lemas, 100)
+    vector_top_palabras =np.array(list(top_palabras.values()))
+    vector_top_lemas =np.array(list(top_lemas.values()))
+    vector_abc =np.array(list(abecedario.values()))
     return vector_top_palabras, vector_top_lemas, vector_abc   
      
   
@@ -21,14 +21,24 @@ def compare(frecuencias1, frecuencias_lemas1,frecuencias2, frecuencias_lemas2,di
                      "entre","hacia", "hasta", "para", "por","según", "sin", "sobre", "tras", "al", "del","y", "e", "ni", "o", "u", "pero", "aunque", 
                      "sino","yo", "tú", "tu", "vos", "él", "ella", "ello","nos", "nosotros", "nosotras","vosotros","vosotras","ellos", "ellas","me", "te",
                      "se", "le", "les", "lo", "la", "los", "las","que","quien","quienes", "cual", "cuales", "cuyo", "cuyos","donde", "cuando", "como", "cuanto",
-                     "ser", "estar", "haber","ya", "muy", "más", "menos", "tan", "todo", "toda", "todos", "todas""cada", "casi", "también", "sí", "no", "si"]
+                     "ser", "estar", "haber","ya", "muy", "más", "menos", "tan", "todo", "toda", "todos", "todas","cada", "casi", "también", "sí", "no", "si"]
+
     for palabra in palabras_vacias:
+    
         if palabra in frecuencias1:
-            del frecuencias_lemas1[palabra]
-        if palabra in frecuencias2:
-            del frecuencias_lemas2[palabra]
-        if palabra in frecuencias2:
             del frecuencias1[palabra]
+            
+        if palabra in frecuencias2:
+            del frecuencias2[palabra]
+            
+        if palabra in frecuencias_lemas1:
+            del frecuencias_lemas1[palabra]
+                
+        if palabra in frecuencias_lemas2:
+            del frecuencias_lemas2[palabra]
+        
+            
+        
  
     numero_palabras_diccionario1, palabras_totales1,numero_palabras_distintas1,numero_lemas_distintos1,numero_palabras_unicas1, numero_lemas_unicos1, top_palabras1, top_lemas1, abecedario1, nuevos_caracteres1 =make_statistics(frecuencias1, frecuencias_lemas1, diccionario, libro_en_string1)
     numero_palabras_diccionario2, palabras_totales2,numero_palabras_distintas2,numero_lemas_distintos2,numero_palabras_unicas2, numero_lemas_unicos2, top_palabras2, top_lemas2, abecedario2, nuevos_caracteres2 =make_statistics(frecuencias2, frecuencias_lemas2, diccionario, libro_en_string2)
@@ -49,14 +59,15 @@ def compare(frecuencias1, frecuencias_lemas1,frecuencias2, frecuencias_lemas2,di
     numero_lemas_totales1=sum(list(frecuencias_lemas1.values()))
     numero_lemas_totales2=sum(list(frecuencias_lemas2.values()))
     
-    puntuacion1=coseno_angulo(vector_top_palabras1, vector_top_palabras2)
+    #puntuacion1=coseno_angulo(vector_top_palabras1, vector_top_palabras2)
     puntuacion2=coseno_angulo(vector_top_lemas1, vector_top_lemas2)
     puntuacion3=coseno_angulo(vector_abc1, vector_abc2)
-    puntuacion4= 1- abs(rpd1 - rpd2)
+    #puntuacion4= 1- abs(rpd1 - rpd2)
     puntuacion5= 1- abs(rpu1 - rpu2)
     puntuacion6= 1 - abs(rld1 - rld2)
-    puntuacion7= 1- abs((numero_palabras_nuevas1/palabras_totales1)-(numero_palabras_nuevas2/palabras_totales2))
+    #puntuacion7= 1- abs((numero_palabras_nuevas1/palabras_totales1)-(numero_palabras_nuevas2/palabras_totales2))
     
-    puntuacion_total= (puntuacion1+ puntuacion2 + puntuacion3+ puntuacion4+ puntuacion5 + puntuacion6+ puntuacion7)*(100/7)
+    #puntuacion_total= (puntuacion1+ puntuacion2 + puntuacion3+ puntuacion4+ puntuacion5 + puntuacion6+ puntuacion7)*(100/7)
+    puntuacion_total= (puntuacion2*70 + puntuacion3*10+ puntuacion5*10 + puntuacion6*10)
     
     return puntuacion_total
